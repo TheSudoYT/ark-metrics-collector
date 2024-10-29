002801bc9dd3316a62336b1b971bf642a110f3fd
Joshua Tracy <55331456+Josh-Tracy@users.noreply.github.com>
2024-10-29 13:09:22 -0400

    pypi(pypi): Publishing workflow created

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     name="ark_metrics_collector",
     version="0.1.0",
     packages=find_packages(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     install_requires=[
         "Flask",
         "prometheus_client",
