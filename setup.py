47989e88751eeb25a23087fe6b4626ee6500a782
Joshua Tracy <55331456+Josh-Tracy@users.noreply.github.com>
2024-10-30 08:40:29 -0400

    docs(readme): Update readme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ark_metrics_collector",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
